31de35c5bf55a32dce9894d32e9b50bef5863cf5
CainHappyfish <119086094+CainHappyfish@users.noreply.github.com>
2024-10-06 22:05:39 +0800

    update: carousel & accordion

---
+++ Doki-UI/src/components/carousel/dokiCarousel.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+import DokiCarouselItem from "./global/dokiCarouselItem.vue";
+</script>
+
+<template>
+  <doki-carousel-item></doki-carousel-item>
+</template>
+
+<style scoped>
+
+</style>
