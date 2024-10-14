488f36368db696d6efda3c563106a95fefcd9c0c
CainHappyfish <280971620@qq.com>
2024-10-14 19:29:28 +0800

    update: divider

---
+++ Doki-UI/src/components/divider/dokiDivider.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import {computed, onMounted, ref, useAttrs, useSlots} from "vue";
+
+const type = ref(useAttrs().type ? "doki-divider__" + useAttrs().type as string : "doki-divider__horizontal")
+const dividerStyle = ref(useAttrs().dividerStyle ? useAttrs().dividerStyle : "solid")
+const contentPosition = computed(() => {
+  if (useAttrs().contentPosition === "left") {
+    return "content__left"
+  } else if (useAttrs().contentPosition === "right") {
+    return "content__right"
+  } else {
+    return ""
+  }
+})
+
+const slots = useSlots().default
+
+</script>
+
+<template>
+  <div class="doki-divider" :class="type">
+    <div class="doki-divider__left" :style="{borderStyle: dividerStyle}" :class="contentPosition"></div>
+    <div class="doki-divider__content" v-if="slots">
+      <slot></slot>
+    </div>
+    <div class="doki-divider__right" :style="{borderStyle: dividerStyle}"></div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
