8072eb0f48121b2fbc05eb1df16b1db289624fc2
CainHappyfish <119086094+CainHappyfish@users.noreply.github.com>
2024-10-15 10:46:55 +0800

    Update dokiAlert.vue

--- Doki-UI/src/components/alert/dokiAlert.vue
+++ Doki-UI/src/components/alert/dokiAlert.vue
@@ -41,7 +41,7 @@ const handleClick = () => {
       </div>
     </div>
     <svg @click="handleClick" t="1728033233491"
-         viewBox="0 0 1024 1024" version="1.1" 
+         viewBox="0 0 1024 1024" version="1.1"
          mlns="http://www.w3.org/2000/svg" p-id="4257" xmlns:xlink="http://www.w3.org/1999/xlink"
          width="50" height="50">
       <path d="M557.248 512l265.408 265.344a32 32 0 0 1-45.312 45.312L512 557.248l-265.344 265.408a32 32 0 0 1-45.312-45.312L466.752 512 201.344 246.656a32 32 0 1 1 45.312-45.312L512 466.752l265.344-265.408a32 32 0 0 1 45.312 45.312L557.248 512z" fill="#ffffff" p-id="4258"></path>
